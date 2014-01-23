4f8619ac6770ad6b94c17f3379118b805191d298
Chris Paveglio <chrispav@ptd.net>
2014-01-23 13:21:29 -0500

    Initial Commit

---
+++ AssistireMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AssistireMe
+//
+//  Created by Chris Paveglio on 1/23/14.
+//  Copyright (c) 2014 none. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AssistireMe/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  AssistireMe
+//
+//  Created by Chris Paveglio on 1/23/14.
+//  Copyright (c) 2014 none. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
